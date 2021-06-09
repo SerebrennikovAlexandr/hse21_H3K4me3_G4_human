58066bd4b1d2762cfcede99667ba48e19e3d431b
Serebrennikov Alexander <adsereb1@mts.ru>
2021-06-09 10:20:28 +0300

    Добавил скрипт и графики с исследованием пиков гистоновой метки

---
+++ src/chip_seeker.R
@@ -0,0 +1,20 @@
+
+NAME <- 'H3K4me3_H1.ENCFF236DPM.hg19.filtered'
+#NAME <- 'H3K4me3_H1.ENCFF178RTX.hg19.filtered'
+
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+# peak <- readPeakFile(BED_FN)
+# pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+# covplot(peak, weightCol="V5")
+# dev.off()
--- src/lib.R
+++ src/lib.R
@@ -13,5 +13,20 @@ library(tibble)  # column_to_rownames
 
 ###
 
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install("ChIPseeker")
+#BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene", force=TRUE)
+#BiocManager::install("clusterProfiler")
+#BiocManager::install("GenomicFeatures", force=TRUE)
+
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(clusterProfiler)
+library(GenomicFeatures)
+library(org.Hs.eg.db)
+
+###
+
 DATA_DIR <- '../data/'
 OUT_DIR <- '../images/'
